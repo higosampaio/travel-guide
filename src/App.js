cd44ee132dc3122ff2a6cd7c13561ed7643ff4db
Higo Sampaio <hglbsm@gmail.com>
2022-04-19 21:03:22 -0300

    feat: add List component filters

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ const App = () => {
     const [bounds, setBounds] = useState({})
     const [childClicked, setChildClicked] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
+    const [type, setType] = useState('restaurants')
+    const [rating, setRating] = useState('')
+    const [filteredPlaces, setFilteredPlaces] = useState([])
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(
@@ -22,13 +25,20 @@ const App = () => {
     }, [])
 
     useEffect(() => {
+        const filteredPlaces = places.filter((place) => place.rating > rating)
+
+        setFilteredPlaces(filteredPlaces)
+    }, [rating])
+
+    useEffect(() => {
         setIsLoading(true)
 
-        getPlacesData(bounds.sw, bounds.ne).then((data) => {
+        getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
             setPlaces(data)
+            setFilteredPlaces([])
             setIsLoading(false)
         })
-    }, [coordinates, bounds])
+    }, [type, coordinates, bounds])
 
     return (
         <>
@@ -37,9 +47,13 @@ const App = () => {
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
                     <List
-                        places={places}
+                        places={filteredPlaces.length ? filteredPlaces : places}
                         childClicked={childClicked}
                         isLoading={isLoading}
+                        type={type}
+                        setType={setType}
+                        rating={rating}
+                        setRating={setRating}
                     />
                 </Grid>
                 <Grid item xs={12} md={8}>
@@ -47,7 +61,7 @@ const App = () => {
                         setCoordinates={setCoordinates}
                         coordinates={coordinates}
                         setBounds={setBounds}
-                        places={places}
+                        places={filteredPlaces.length ? filteredPlaces : places}
                         setChildClicked={setChildClicked}
                     />
                 </Grid>
--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,25 @@
 import axios from 'axios'
 
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
-
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
     try {
         const {
             data: { data },
-        } = await axios.get(URL, {
-            params: {
-                bl_latitude: sw.lat,
-                tr_latitude: ne.lat,
-                bl_longitude: sw.lng,
-                tr_longitude: ne.lng,
-            },
-            headers: {
-                'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
-                'X-RapidAPI-Key':
-                    '5d0683e17emsh0df2860cf1eb185p1946f6jsn53b4e6ee8716',
-            },
-        })
+        } = await axios.get(
+            `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+            {
+                params: {
+                    bl_latitude: sw.lat,
+                    tr_latitude: ne.lat,
+                    bl_longitude: sw.lng,
+                    tr_longitude: ne.lng,
+                },
+                headers: {
+                    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+                    'X-RapidAPI-Key':
+                        '5d0683e17emsh0df2860cf1eb185p1946f6jsn53b4e6ee8716',
+                },
+            }
+        )
 
         return data
     } catch (error) {
